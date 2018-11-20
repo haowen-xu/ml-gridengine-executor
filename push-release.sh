981eed164c1cd2e82dee864e7e543bccd49344a7
Haowen Xu <haowen.xu@outlook.com>
2018-11-20 16:14:37 +0800

    minor update

--- push-release.sh
+++ push-release.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 set -e
-git clone -b release "https://${GH_TOKEN}@github.com/haowen-xu/ml-gridengine-executor.git" /tmp/ml-gridengine-executor
-cp build/ml-gridengine-executor /tmp/ml-gridengine-executor/
+WORKDIR=/tmp/ml-gridengine-executor/
+
+# get the last release
+git clone -b release "https://${GH_TOKEN}@github.com/haowen-xu/ml-gridengine-executor.git" "${WORKDIR}"
+
+# deploy the main executable
+cp build/ml-gridengine-executor "${WORKDIR}"
+
+# deploy the tests
+mkdir -p "${WORKDIR}/tests/"
+cp build/ml-gridengine-executor-unit-tests "${WORKDIR}/tests/unit-tests"
+cp -R tests/integrated-tests "${WORKDIR}/tests/"
+
+# push to release branch
 cd /tmp/ml-gridengine-executor/
 git add --all .
 if git commit -m "Travis CI Auto Builder"; then
