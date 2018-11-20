8ff19a3a796dd4717f4b321469f38a3e8183a7f1
Haowen Xu <haowen.xu@outlook.com>
2018-11-20 14:54:14 +0800

    minor update

--- build.sh
+++ build.sh
@@ -3,18 +3,17 @@
 sudo docker run -ti --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" \
     haowenxu/static-cpp-build \
     sh -c '
-        rm -rf build && \
-            mkdir -p build && \
-            cd build && \
-            cmake \
-                -DCMAKE_C_COMPILER=/usr/bin/x86_64-alpine-linux-musl-gcc \
-                -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-alpine-linux-musl-g++ \
-                -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" \
-                -DCMAKE_EXE_LINKER_FLAGS="-static" \
-                -DBUILD_SHARED_LIBS=off \
-                -DCMAKE_BUILD_TYPE=Release \
-                .. && \
-            make VERBOSE=1 && \
-            cp ml_gridengine_executor ml-gridengine-executor && \
-            strip -s ml-gridengine-executor
+        mkdir -p build && \
+        cd build && \
+        cmake \
+            -DCMAKE_C_COMPILER=/usr/bin/x86_64-alpine-linux-musl-gcc \
+            -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-alpine-linux-musl-g++ \
+            -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" \
+            -DCMAKE_EXE_LINKER_FLAGS="-static" \
+            -DBUILD_SHARED_LIBS=off \
+            -DCMAKE_BUILD_TYPE=Release \
+            .. && \
+        make VERBOSE=1 && \
+        cp ml_gridengine_executor ml-gridengine-executor && \
+        strip -s ml-gridengine-executor
     '
